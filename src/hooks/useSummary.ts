50d353b5d9c8b2779b4aea6202021f1d8f07474f
Daniel Scharp <daniel.scharp@hotmail.com>
2024-02-02 16:57:07 -0300

    memo, useMemo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,5 +1,6 @@
 import { TransactionsContext } from "../contexts/TransactionsContext"
 import { useContextSelector } from "use-context-selector"
+import { useMemo } from "react";
 
 export function useSummary () {
     const transactions = useContextSelector(TransactionsContext, 
@@ -8,23 +9,25 @@ export function useSummary () {
         }
     )
 
-    const summury = transactions.reduce(
-        (acc, transaction) => {
-            if (transaction.type === 'income' ){
-                acc.income += transaction.price
-                acc.total += transaction.price
+    const summury = useMemo(() => {
+        transactions.reduce(
+            (acc, transaction) => {
+                if (transaction.type === 'income' ){
+                    acc.income += transaction.price
+                    acc.total += transaction.price
+                    
+                } else {
+                    acc.outcome += transaction.price
+                    acc.total -= transaction.price
+                }
                 
-            } else {
-                acc.outcome += transaction.price
-                acc.total -= transaction.price
+                return acc
+            }, 
+            {
+                income: 0, outcome: 0, total: 0
             }
-            
-            return acc
-        }, 
-        {
-            income: 0, outcome: 0, total: 0
-        }
-    )
+        )
+    }, [transactions])
 
     return summury;
 }
